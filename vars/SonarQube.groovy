dc83cec405d797fd5cd91701ec7c0ec4a3759cc1
sandeepds <sandeepds2002@gmail.com>
2019-08-29 18:55:00 +0530

    Update SonarQube.groovy

--- vars/SonarQube.groovy
+++ vars/SonarQube.groovy
@@ -1,18 +1,15 @@
 def call(script) {
-  stage('Sonarqube-scan'){
-        //Run the sonar scan
-        withSonarQubeEnv('sonarqube'){
-                sh 'mvn clean package sonar:sonar'
-        }
-   }
-
-   stage('Quality-Gate') {
-        timeout(time: 1, unit: 'MINUTES') {
-        def qg = waitForQualityGate()
-          if (qg.status != 'OK') {
-                currentBuild.status='FAILURE'
-                error "Pipeline aborted due to quality gate failure: ${qg.status}"
-           }
-        }
+  
+    //Run the sonar scan
+    withSonarQubeEnv('sonarqube'){
+            sh 'mvn clean package sonar:sonar'
+    }
+    //Wait for response
+    timeout(time: 1, unit: 'MINUTES') {
+    def qg = waitForQualityGate()
+      if (qg.status != 'OK') {
+            currentBuild.status='FAILURE'
+            error "Pipeline aborted due to quality gate failure: ${qg.status}"
+       }
    }
 }
