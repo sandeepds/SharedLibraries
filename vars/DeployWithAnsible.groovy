7e8c85f58aaad6a7b0c8eaeefb23c875d08eb153
sandeepds <sandeepds2002@gmail.com>
2019-08-30 16:27:17 +0530

    Update DeployWithAnsible.groovy

--- vars/DeployWithAnsible.groovy
+++ vars/DeployWithAnsible.groovy
@@ -1,6 +1,6 @@
 def call(script){
   //def user = "devopsinfra"
   //def pass = "Husslefree12&#36;1"
-  sh {WORKSPACE}
-  //sh "ansible-playbook ${WORKSPACE}/Devops_Piepline/spring-playbook.yaml -i inventory -vvvv "
+  //sh {WORKSPACE}
+  sh "ansible-playbook ${WORKSPACE}/Devops_Piepline/spring-playbook.yaml -vvvv "
 }
