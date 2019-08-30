e5b671997d188545d5ba4af6e40b41328a3e15b7
sandeepds <sandeepds2002@gmail.com>
2019-08-30 15:17:03 +0530

    Update DeployWithAnsible.groovy

--- vars/DeployWithAnsible.groovy
+++ vars/DeployWithAnsible.groovy
@@ -1,5 +1,5 @@
 def call(script){
   //def user = "devopsinfra"
   //def pass = "Applestore12\$6"
-  sh "ansible-playbook /var/lib/jenkins/workspace/Devops_Piepline/spring-playbook.yaml.yml -i inventory -vvvv "
+  sh "ansible-playbook /var/lib/jenkins/workspace/Devops_Piepline/spring-playbook.yaml -i inventory -vvvv "
 }
