ab5737784fd1b4753225bfdcc8cc12b6ed663352
sandeepds <sandeepds2002@gmail.com>
2019-08-30 13:25:03 +0530

    Update BuildWithM3.groovy

--- vars/BuildWithM3.groovy
+++ vars/BuildWithM3.groovy
@@ -1,17 +1,17 @@
 //def mvnHome = tool 'M3'
 def call(script, mavenVar) {
-    //script.withMaven (
-        //maven: mavenVar) {
-                //sh 'mvn clean install'
-    //}
+    script.withMaven (
+        maven: mavenVar) {
+                sh 'mvn clean install'
+    }
     
     // Run the maven build
     //withEnv(["MVN_HOME=$mvnHome"]) {
-    withEnv(["MVN_HOME=$mavenVar"]) {
-       if (isUnix()) {
-          sh '"$MVN_HOME/bin/mvn" -Dmaven.test.failure.ignore clean package'
-       } else {
-          bat(/"%MVN_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
-       }
-    }
+    //withEnv(["MVN_HOME=$mavenVar"]) {
+       //if (isUnix()) {
+          //sh '"$MVN_HOME/bin/mvn" -Dmaven.test.failure.ignore clean package'
+       //} else {
+          //bat(/"%MVN_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+       //}
+    //}
 }
