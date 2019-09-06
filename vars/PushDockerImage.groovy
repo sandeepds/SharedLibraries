d7b4e220622b321601aa84c4e7a4bece69bd1731
sandeepds <sandeepds2002@gmail.com>
2019-09-06 16:41:26 +0530

    Update PushDockerImage.groovy

--- vars/PushDockerImage.groovy
+++ vars/PushDockerImage.groovy
@@ -1,4 +1,4 @@
 def call(script) {
-  sh "sudo docker build -t sandeepds2002/petclinic ."
+  sh "sudo docker build -t sandeepds2002/petclinic_${BUILD_NUMBER} ."
   sh "sudo docker push sandeepds2002/petclinic"
 }
